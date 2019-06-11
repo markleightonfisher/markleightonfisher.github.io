4979d0151032c0ebaa0969d3596f9f9f929c9c33
Mark Leighton Fisher <markleightonfisher@gmail.com>
2019-06-11 07:10:04 -0400

    Update left menu

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,6 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
-        "tags": ["Jekyll","update"],
-        "url": "http://localhost:4000/blog/welcome-to-jekyll/",
-        "teaser":null},{
         "title": "Roles And Responsibilities In Scrum",
         "excerpt":"Roles and Responsibilities in Scrum I want to make explicit what was only implied in Myth: The Product Backlog is Maintained Exclusively by the Product Owner – that every action and every issue in a Sprint is the responsibility of the whole Scrum team. That being said, at the very...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/roles-and-responsibilities-in-scrum/",
+        "url": "/roles-and-responsibilities-in-scrum/",
         "teaser":null}]
